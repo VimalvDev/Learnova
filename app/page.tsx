abe91ce7dc8d5c5cdae521abe41d363e094ca38d
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2026-03-23 04:02:34 +0530

    fixed minor error

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 import Navbar from "@/components/common/Navbar";
 import Hero from "@/components/home/hero/Hero";
 import HowItWorks from "@/components/home/howitworks/HowItWorks";
-
-import WeaknessDetection from "@/components/WeaknessDetection";
-import RagShowcase from "@/components/RagShowcase";
-import Analytics from "@/components/Analytics";
-import KnowledgeGraph from "@/components/KnowledgeGraph";
-import Pricing from "@/components/Pricing";
 import FAQ from "@/components/home/FAQ";
-import FinalCTA from "@/components/home/CTA";
-
 import Manifesto from "@/components/home/Manifesto";
 import Features from "@/components/home/features/Features";
 import Footer from "@/components/common/Footer";
